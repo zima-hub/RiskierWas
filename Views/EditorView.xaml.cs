53906f7668de15abac2a1501a029d61077fd5727
Matthias Zimmer <zi@gsm-sha.de>
2025-08-18 00:05:20 +0200

    Fragen erweitert

--- Views/EditorView.xaml.cs
+++ Views/EditorView.xaml.cs
@@ -8,5 +8,10 @@ namespace RiskierWas.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
     }
 }
